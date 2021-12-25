9e5c9d2709a2cce31522858f7c590433adee515d
Daffa Z <67418594+daffaz@users.noreply.github.com>
2021-12-25 18:51:28 +0700

    personal change

--- 02-write-your-first-program/exercises/01-print-names/main.go
+++ 02-write-your-first-program/exercises/01-print-names/main.go
@@ -5,8 +5,9 @@
 // For more tutorials  : https://learngoprogramming.com
 // In-person training  : https://www.linkedin.com/in/inancgumus/
 // Follow me on twitter: https://twitter.com/inancgumus
-
 package main
+
+import "fmt"
 
 // ---------------------------------------------------------
 // EXERCISE: Print names
@@ -24,6 +25,6 @@ package main
 // ---------------------------------------------------------
 
 func main() {
-	// ?
-	// ?
+	fmt.Println("Hello wawan")
+	fmt.Println("Hello wawan")
 }
---
+++ 03-packages-and-scopes/exercises/02-scopes/printer.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func hello() {
+	fmt.Println("Daffa")
+	bye()
+}
